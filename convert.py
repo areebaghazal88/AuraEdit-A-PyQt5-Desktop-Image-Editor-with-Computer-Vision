9d0463cd60ffcf16eabe911d1ad5e9860a79d2d4
areebaghazal88 <areebaghazal88@gmail.com>
2025-05-17 10:14:58 +0500

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,8 +1,8 @@
 from PyQt5 import uic
 
 # Specify the input .ui file and output .py file
-ui_file = "mydesign2.ui"
-output_file = "mydesign2.py"
+ui_file = "mydesign2or.ui"
+output_file = "mydesign2or.py"
 
 # Compile the .ui file into a .py file
 with open(output_file, "w") as py_file:
